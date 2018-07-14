7d537011fc36c14bf842e716b2233532aafa8c88
fermi_dirac <ManueleBryan@gmail.com>
2018-07-14 00:12:35 -0700

    css modules work

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import style from './App.module.css';
 
-console.log(style);
-
 class App extends Component {
   render() {
     return (
-      <div className={style.App}>
+      <div className={style['App']}>
         <header className={style['App-header']}>
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className={style['App-logo']} alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
