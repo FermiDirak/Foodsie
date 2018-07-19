b6e39608d884e04d636646cf027f5c0815a3447f
fermi_dirac <ManueleBryan@gmail.com>
2018-07-18 23:53:48 -0700

    added review text and it looks sexy

--- client/src/components/RestaurantsFeedItem/RestaurantsFeedItem.jsx
+++ client/src/components/RestaurantsFeedItem/RestaurantsFeedItem.jsx
@@ -89,6 +89,15 @@ class RestaurantsFeedItem extends Component {
     );
   }
 
+  renderTopReview = () => {
+
+    return (
+      <div className={styles['top-review']}>
+        Jasmine says: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      </div>
+    );
+  }
+
   render() {
     const { restaurant } = this.props;
 
@@ -109,8 +118,9 @@ class RestaurantsFeedItem extends Component {
           <div className={styles['statistics-row']}>
             { this.renderRatingStars() }
             { this.renderPrice() }
-
           </div>
+
+          { this.renderTopReview() }
 
         </div>
 
