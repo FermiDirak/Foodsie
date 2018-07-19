c7fda6b87cfb9431e0ff6f6630ab448dc46beb54
fermi_dirac <ManueleBryan@gmail.com>
2018-07-19 11:28:49 -0700

    added styler to google maps

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -78,9 +78,65 @@ class Map extends Component {
         centerAroundCurrentLocation={true}
         onClick={this.fetchPlaces}
         onReady={this.fetchPlaces}
-        clickableIcons={true}
+        clickableIcons={false}
         disableDefaultUI={true}
-      />
+        styles={[
+          {
+            "featureType": "administrative.land_parcel",
+            "elementType": "labels",
+            "stylers": [
+              {
+                "visibility": "off"
+              }
+            ]
+          },
+          {
+            "featureType": "poi",
+            "elementType": "labels.text",
+            "stylers": [
+              {
+                "visibility": "off"
+              }
+            ]
+          },
+          {
+            "featureType": "poi.business",
+            "stylers": [
+              {
+                "visibility": "off"
+              }
+            ]
+          },
+          {
+            "featureType": "road",
+            "elementType": "labels.icon",
+            "stylers": [
+              {
+                "visibility": "off"
+              }
+            ]
+          },
+          {
+            "featureType": "road.local",
+            "elementType": "labels",
+            "stylers": [
+              {
+                "visibility": "off"
+              }
+            ]
+          },
+          {
+            "featureType": "transit",
+            "stylers": [
+              {
+                "visibility": "off"
+              }
+            ]
+          }
+        ]}
+      >
+
+      </GoogleMap>
     );
   }
 }
