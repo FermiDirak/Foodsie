e5d32048115a2aa33137dcc86cc96f8f77b9e453
fermi_dirac <ManueleBryan@gmail.com>
2018-07-24 15:14:28 -0700

    created boilerplate for stars and price displays

---
+++ client/src/components/PriceDisplay/PriceDisplay.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './PriceDisplay.module.css';
+
+class PriceDisplay extends Component {
+  state = {
+
+  };
+
+  render() {
+    return (
+      <div className={styles['container']}>
+
+      </div>
+    );
+  }
+}
+
+export default PriceDisplay;
---
+++ client/src/components/StarsDisplay/StarsDisplay.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './StarsDisplay.module.css';
+
+class StarsDisplay extends Component {
+  state = {
+
+  };
+
+  render() {
+    return (
+      <div className={styles['container']}>
+
+      </div>
+    );
+  }
+}
+
+export default StarsDisplay;
